c761e64fbfdc38efb7692c7731dc33008dba71df
OscarClemente <oscar.clemente@gmail.com>
2023-12-19 18:19:12 +0100

    feat: Redo from scratch

--- ch2/numpyLayer.py
+++ ch2/numpyLayer.py
@@ -1,13 +1,10 @@
 import numpy as np
 
-inputs = [[1.0, 2.0, 3.0, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
+inputs = [1.0, 2.0, 3.0, 2.5]
 weights = [[0.2, 0.8, -0.5, 1.0],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87]]
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
 biases = [2.0, 3.0, 0.5]
 
-layerOutputs = np.dot(inputs, np.array(weights).T) + biases
-
-print(layerOutputs)
+outputs = np.dot(weights, inputs) + biases
+print(outputs)
