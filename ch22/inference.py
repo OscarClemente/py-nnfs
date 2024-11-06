5b3cd46e7cbf1d79263579b29a3a469e61c23c71
OscarClemente <oscar.clemente@gmail.com>
2024-11-06 15:54:03 +0100

    feat: Inference works

--- ch22/inference.py
+++ ch22/inference.py
@@ -708,6 +708,19 @@ if not os.path.isdir(FOLDER):
         zip_images.extractall(FOLDER)
     print('Done!')
 
+fashion_mnist_labels = {
+    0: 'T-shirt/top',
+    1: 'Trouser',
+    2: 'Pullover',
+    3: 'Dress',
+    4: 'Coat',
+    5: 'Sandal',
+    6: 'Shirt',
+    7: 'Sneaker',
+    8: 'Bag',
+    9: 'Ankle boot'
+}
+
 x, y, x_test, y_test = create_data_mnist(FOLDER)
 x = (x.astype(np.float32) - 127.5) / 127.5
 x_test = (x_test.astype(np.float32) - 127.5) / 127.5
@@ -722,5 +735,25 @@ y = y[keys]
 
 
 model = Model.load('fashion_mnist.model')
-confidences = model.predict(x_test[:5])
-print(confidences)
+
+# tshirt.png prediction
+image_data = cv2.imread('tshirt.png', cv2.IMREAD_GRAYSCALE)
+image_data = cv2.resize(image_data, (28, 28))
+image_data = 255-image_data
+image_data = (image_data.reshape(1, -1).astype(np.float32) - 127.5) / 127.5
+
+confidences = model.predict(image_data)
+predictions = model.output_layer_activation.predictions(confidences)
+prediction = fashion_mnist_labels[predictions[0]]
+print(prediction)
+
+# pants.png prediction
+image_data = cv2.imread('pants.png', cv2.IMREAD_GRAYSCALE)
+image_data = cv2.resize(image_data, (28, 28))
+image_data = 255-image_data
+image_data = (image_data.reshape(1, -1).astype(np.float32) - 127.5) / 127.5
+
+confidences = model.predict(image_data)
+predictions = model.output_layer_activation.predictions(confidences)
+prediction = fashion_mnist_labels[predictions[0]]
+print(prediction)
